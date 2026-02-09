565e7453ef9a6bade32898b6c0b9cf26948db729
SShakusora <1404928002@qq.com>
2026-02-09 23:15:27 +0800

    feat: Replace Files.list with Files.walk to support subdirectories.

--- src/main/java/com/sshakusora/create_schematic_i18n/CSI18N.java
+++ src/main/java/com/sshakusora/create_schematic_i18n/CSI18N.java
@@ -1,11 +1,14 @@
 package com.sshakusora.create_schematic_i18n;
 
 
+import com.mojang.logging.LogUtils;
 import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
 
 @Mod(CSI18N.MODID)
 public class CSI18N {
     public static final String MODID = "create_schematic_i18n";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public CSI18N() {}
 }
--- src/main/java/com/sshakusora/create_schematic_i18n/mixin/ClientSchematicLoaderMixin.java
+++ src/main/java/com/sshakusora/create_schematic_i18n/mixin/ClientSchematicLoaderMixin.java
@@ -1,6 +1,7 @@
 package com.sshakusora.create_schematic_i18n.mixin;
 
 import com.simibubi.create.content.schematics.client.ClientSchematicLoader;
+import com.sshakusora.create_schematic_i18n.CSI18N;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -9,11 +10,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Mixin(ClientSchematicLoader.class)
 public class ClientSchematicLoaderMixin {
@@ -22,17 +21,25 @@ public class ClientSchematicLoaderMixin {
     @Inject(method = "refresh", at = @At(value = "INVOKE", target = "Ljava/util/List;sort(Ljava/util/Comparator;)V"), remap = false)
     private void beforeSort(CallbackInfo ci) {
         this.availableSchematics.clear();
-        try {
-            Files.list(Paths.get("schematics/")).filter((f) -> !Files.isDirectory(f, new LinkOption[0]) && f.getFileName().toString().endsWith(".nbt")).forEach((path) -> {
-                if (!Files.isDirectory(path, new LinkOption[0])) {
-                    String rawName = path.getFileName().toString();
-                    String key = rawName.endsWith(".nbt") ? rawName.substring(0, rawName.length() - 4) : rawName;
-                    this.availableSchematics.add(Component.translatable(key));
-                }
-            });
-        } catch (NoSuchFileException var2) {
+        Path rootPath = Paths.get("schematics/");
+        Path excludePath = rootPath.resolve("uploaded");
+
+        try (Stream<Path> stream = Files.walk(rootPath)) {
+            stream.filter(path -> {
+                        if (Files.isDirectory(path)) return false;
+                        if (path.startsWith(excludePath)) return false;
+                        return path.getFileName().toString().endsWith(".nbt");
+                    })
+                    .forEach(path -> {
+                        Path relativePath = rootPath.relativize(path);
+                        String rawName = relativePath.toString().replace("\\", ".");
+                        String key = rawName.endsWith(".nbt") ? rawName.substring(0, rawName.length() - 4) : rawName;
+
+                        this.availableSchematics.add(Component.translatable(key));
+                    });
+        } catch (NoSuchFileException ignored) {
         } catch (IOException e) {
-            e.printStackTrace();
+            CSI18N.LOGGER.error("Failed to refresh schematics", e);
         }
     }
 }
