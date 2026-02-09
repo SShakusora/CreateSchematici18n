c568ce376ddf2ba4a0c478536e4bab34cd13e6c3
SShakusora <1404928002@qq.com>
2026-02-09 23:15:21 +0800

    feat: Replace Files.list with Files.walk to support subdirectories.

--- src/main/java/com/sshakusora/create_schematic_i18n/mixin/ClientSchematicLoaderMixin.java
+++ src/main/java/com/sshakusora/create_schematic_i18n/mixin/ClientSchematicLoaderMixin.java
@@ -9,11 +9,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Mixin(ClientSchematicLoader.class)
 public class ClientSchematicLoaderMixin {
@@ -22,15 +20,23 @@ public class ClientSchematicLoaderMixin {
     @Inject(method = "refresh", at = @At(value = "INVOKE", target = "Ljava/util/List;sort(Ljava/util/Comparator;)V"), remap = false)
     private void beforeSort(CallbackInfo ci) {
         this.availableSchematics.clear();
-        try {
-            Files.list(Paths.get("schematics/")).filter((f) -> !Files.isDirectory(f, new LinkOption[0]) && f.getFileName().toString().endsWith(".nbt")).forEach((path) -> {
-                if (!Files.isDirectory(path, new LinkOption[0])) {
-                    String rawName = path.getFileName().toString();
-                    String key = rawName.endsWith(".nbt") ? rawName.substring(0, rawName.length() - 4) : rawName;
-                    this.availableSchematics.add(Component.translatable(key));
-                }
-            });
-        } catch (NoSuchFileException var2) {
+        Path rootPath = Paths.get("schematics/");
+        Path excludePath = rootPath.resolve("uploaded");
+
+        try (Stream<Path> stream = Files.walk(rootPath)) {
+            stream.filter(path -> {
+                        if (Files.isDirectory(path)) return false;
+                        if (path.startsWith(excludePath)) return false;
+                        return path.getFileName().toString().endsWith(".nbt");
+                    })
+                    .forEach(path -> {
+                        Path relativePath = rootPath.relativize(path);
+                        String rawName = relativePath.toString().replace("\\", ".");
+                        String key = rawName.endsWith(".nbt") ? rawName.substring(0, rawName.length() - 4) : rawName;
+
+                        this.availableSchematics.add(Component.translatable(key));
+                    });
+        } catch (NoSuchFileException ignored) {
         } catch (IOException e) {
             e.printStackTrace();
         }
